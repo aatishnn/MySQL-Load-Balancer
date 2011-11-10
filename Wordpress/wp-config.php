56dffb0573f393fa24b9f1330a71107e71250b3b
Aatish Neupane <aatishnn@gmail.com>
2011-11-10 09:01:12 +0545

    Added License and Simple Ready Configuration for Wordpress

---
+++ Wordpress/wp-config.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * MySQL Load Balancer - Aatish Neupane 
+ * A simple anywhere usable load balancer for MySQL servers.For use with Wordpress.
+ * 
+ * Copyright (c) 2011 Aatish Neupane
+ *
+ *  This file is part of MySQL Load Balancer.
+
+    MySQL Load Balancer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MySQL Load Balancer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MySQL Load Balancer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * The base configurations of the WordPress.
+ *
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * Secret Keys, WordPress Language, and ABSPATH. You can find more information
+ * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
+ * wp-config.php} Codex page. You can get the MySQL settings from your web host.
+ *
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
+ *
+ * @package WordPress
+ */
+
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+require("balancer.inc.php");
+$details = new loadbalancer();
+$server=$details->returnconfig();
+define('DB_NAME', $server[3]);
+
+/** MySQL database username */
+define('DB_USER', $server[1]);
+
+/** MySQL database password */
+define('DB_PASSWORD', $server[2]');
+
+/** MySQL hostname */
+define('DB_HOST', $server[0]);
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * WordPress Localized Language, defaults to English.
+ *
+ * Change this to localize WordPress. A corresponding MO file for the chosen
+ * language must be installed to wp-content/languages. For example, install
+ * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
+ * language support.
+ */
+define('WPLANG', '');
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ balancer.inc.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * MySQL Load Balancer - Aatish Neupane 
+ * A simple anywhere usable load balancer for MySQL servers
+ * 
+ * Copyright (c) 2011 Aatish Neupane
+ *
+ *  This file is part of MySQL Load Balancer.
+
+    MySQL Load Balancer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MySQL Load Balancer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MySQL Load Balancer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+class loadbalancer
+{
+public $server_main=array("Hostname:Port","root","password","dbname");
+public $backup_server=array("Hostname:Port","root","password","dbname");
+public $debug_info = "";
+
+function checkserver($server,$username,$password,$db) {
+	try{
+			$connection=@mysql_connect($server,$username,$password);
+			if(!$connection)
+			throw new Exception("Cannot connect to MYSQL server");
+
+			$db_select=@mysql_select_db($db,$connection);
+			if(!$db_select)
+			throw new Exception("Cannot select database");
+			return true;
+		}
+	catch(Exception $e){
+			$this->debug_info = $e->getMessage();
+			return false;
+			}
+
+	}
+
+function returnconfig(){
+if(checkserver($this->server_main[0],$this->server_main[1],$this->server_main[2],$this->server_main[3])
+return server_main;
+else
+return backup_server;
+}
+}
+?>
+
